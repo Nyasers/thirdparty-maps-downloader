0d2b3d4883fa683836721bb86d6f7018f9cc30e2
Nyaser <Nyasers@outlook.com>
2025-10-28 03:25:46 +0800

    fix(自动下载): 优化重定向逻辑并移除冗余延迟
    将重定向逻辑封装在setTimeout中统一处理
    移除内部冗余的500毫秒延迟，简化代码结构

--- assets/main/auto-download.js
+++ assets/main/auto-download.js
@@ -1,11 +1,11 @@
-window.onload = () => {
+window.onload = () => setTimeout(() => {
     // 检查页面中是否存在可点击的下载按钮(a标签)
     // 如果存在，则表示文件可用，执行重定向
     const downloadButton = document.querySelector('a[href="{{finalRedirectUrl}}"]');
 
     if (downloadButton) {
         // 当页面渲染了成功按钮(a标签)时，执行重定向
-        setTimeout(() => window.location = '{{finalRedirectUrl}}', 500); // 500毫秒后执行重定向，确保页面元素渲染完成
+        window.location = '{{finalRedirectUrl}}'
     }
     // 如果页面渲染的是禁用按钮(button标签)，则不执行任何操作
-};
+}, 500);
